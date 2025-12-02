787057053c7cfd255f79634a0f54f6911273932e
Alicja Banaszewska <74204521+Smoka24@users.noreply.github.com>
2025-12-02 14:57:33 +0100

    Starting part 2

---
+++ 2025/Day 1_Part 2.cpp
@@ -0,0 +1,70 @@
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int turnNumber(std::string input)
+{
+    int turnNumber = 0;
+
+    std::string numStr = input.substr(1, input.length() - 1);
+    turnNumber = std::atoi(numStr.c_str());
+
+    return turnNumber;
+}
+
+int main() 
+{
+    std::string path = "input1.txt";
+    std::vector<std::string> inputs;
+    int actualIndex = 50;
+    int numberOfZeros = 0;
+
+    std::ifstream file(path);
+    std::string line = "";
+    while(getline (file, line))
+    {
+        inputs.push_back(line);
+    }
+
+    for(auto input: inputs)
+    {
+        int turns = turnNumber(input);
+
+
+        if(input[0] == 'L')
+        {
+            actualIndex -= turns;
+        
+            if(actualIndex < 0)
+            {
+                int diff = actualIndex % 100 * -1;
+                actualIndex = 100 - diff;
+            }
+        }
+        else
+        {
+            actualIndex += turns;
+        
+            if(actualIndex > 99)
+            {
+                int diff = actualIndex  % 100 - 99;
+                actualIndex = -1 + diff;
+            }
+        }
+        if(actualIndex % 100 == 0)
+        {
+            actualIndex = 0;
+        }
+
+        if(actualIndex == 0)
+        {
+            numberOfZeros++;
+        }
+    }
+
+    std::cout << "Answer: " << numberOfZeros;
+
+
+}
