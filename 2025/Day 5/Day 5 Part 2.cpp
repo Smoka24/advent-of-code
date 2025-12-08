f3721ae810c581fda34e1a486f82cd654e5f7c25
Alicja Banaszewska <74204521+Smoka24@users.noreply.github.com>
2025-12-08 13:58:48 +0100

    Day 5 Part 2 started

---
+++ 2025/Day 5/Day 5 Part 2.cpp
@@ -0,0 +1,87 @@
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::string path = "input.txt";
+    std::string input = "";
+    std::vector<std::string> inputs;
+    std::vector<std::pair<long long, long long>> ranges;
+    std::ifstream file(path);
+    std::string temp = "";
+    bool wasNewLine = false;
+    long long freshIds = 0;
+    long long counter = 0;
+
+    while(getline(file, temp))
+    {
+        if(temp == "")
+        {
+            break;
+        }
+        inputs.push_back(temp);
+    }
+
+    for(auto input: inputs)
+    {
+        ///Checking half of the table with ranges///
+        if(wasNewLine == false)
+        {
+            if(input == "")
+            {
+                wasNewLine = true;
+            }
+            else
+            {
+            long long beginningOfRange = 0;
+            long long endOfRange = 0;
+            for(long long i = 0; i < input.size(); i++)
+            {
+                char c = input[i];
+                if(c == '-')
+                {
+                    try
+                    {                                            
+                        beginningOfRange = std::stoll(input.substr(0, i));
+                        endOfRange = std::stoll(input.substr(i+1, input.size()-1));
+                    }
+                    catch(const std::exception& e)
+                    {
+                        std::cout << e.what() << '\n';
+                        std::cout << "Some shit happened in paring range: " << input << ": " << input.substr(0, i) << " " << input.substr(i+1, input.size()-1) << "\n";
+                    }
+
+                    ranges.push_back(std::make_pair(beginningOfRange, endOfRange));
+                }
+            }
+        }
+        }
+
+    }
+
+    
+
+    for(auto range : ranges)
+    {
+        counter = counter + (range.second - range.first) + 1;
+
+        for(auto j : ranges)
+        {
+            if(range.first < j.second && range.first > j.first)
+            {
+                counter = counter - (j.second - range.first + 1);
+            }
+            ///Answer too High
+            else if(range.second < j.second && range.second > j.first)
+            {
+                counter = counter - (range.second - j.first + 1);
+            }
+            ///Now answer too Low
+        }
+    }
+    std::cout << "Answer: " << counter;
+
+}
